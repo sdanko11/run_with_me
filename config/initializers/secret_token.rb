bbd16772b71b7ec2a05a9f188e778ce04bf0eb13
steve <stevedanko1@gmail.com>
2014-01-11 10:46:22 -0500

    set up working enviorment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RunWithMe::Application.config.secret_key_base = '5bbb7b42af530a5f01b5880bc6f6834b7ad06c20580682dccd2475f2a48f0e090faf1413773aa7e07474cf5cf1b4ff668c6aae97cea3fa9640737178b9841bcb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RunWithMe::Application.config.session_store :cookie_store, key: '_run_with_me_session'
